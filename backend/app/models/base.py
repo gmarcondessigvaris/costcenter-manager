161db452ef44bbbb9c3bc0dc62fb91957fd241ff
gmarcondessigvaris <gustavo.marcondes@sigvaris.com>
2026-06-03 16:31:07 +0100

    Initial commit

---
+++ backend/app/models/base.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+from sqlalchemy import Column, DateTime
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class TimestampMixin:
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
